e05e44dc238ac2ba3e976595daffaf93fd5b62dc
Alexis Tabary <alexis.tabary@gmail.com>
2014-09-08 18:29:15 +0900

    Moment.to_iso_string added to generate ISO8601 strings

--- lib/moment.ex
+++ lib/moment.ex
@@ -33,14 +33,36 @@ defmodule Moment do
             nanosecond: microsecond * 1000, offset: offset}
   end
 
+  @doc """
+  Returns the moment as an ISO8601 string.
+  """
+  def to_iso_string(moment) do
+    case moment.offset do
+      0 ->
+        "~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0wZ"
+        |> :io_lib.format([moment.year, moment.month, moment.day, moment.hour, moment.minute, moment.second])
+      _ ->
+        offset_h = div(abs(moment.offset), 60)
+        offset_m = rem(abs(moment.offset), 60)
+        if moment.offset > 0 do
+          "~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w+~.2.0w:~.2.0w"
+        else
+          "~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w-~.2.0w:~.2.0w"
+        end
+        |> :io_lib.format([moment.year, moment.month, moment.day, moment.hour, moment.minute, moment.second,
+                           offset_h, offset_m])
+    end
+    |> :erlang.iolist_to_binary()
+  end
+
   defp calc_offset(now) do
     local = now
-      |> :calendar.now_to_local_time
-      |> :calendar.datetime_to_gregorian_seconds
+    |> :calendar.now_to_local_time
+    |> :calendar.datetime_to_gregorian_seconds
 
     universal = now
-      |> :calendar.now_to_universal_time
-      |> :calendar.datetime_to_gregorian_seconds
+    |> :calendar.now_to_universal_time
+    |> :calendar.datetime_to_gregorian_seconds
 
     div(local , 60) - div(universal, 60)
   end
