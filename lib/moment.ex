2ca804ab1c0a3e559f3c0ca03f671cf57c7ac5c9
Alexis Tabary <alexis.tabary@gmail.com>
2014-09-09 11:38:56 +0900

    Basic support for formatting

--- lib/moment.ex
+++ lib/moment.ex
@@ -34,26 +34,77 @@ defmodule Moment do
   end
 
   @doc """
-  Returns the moment as an ISO8601 string.
+  Format moment as an ISO8601 string.
   """
   def to_iso_string(moment) do
-    case moment.offset do
-      0 ->
-        "~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0wZ"
-        |> :io_lib.format([moment.year, moment.month, moment.day, moment.hour, moment.minute, moment.second])
-      _ ->
-        offset_h = div(abs(moment.offset), 60)
-        offset_m = rem(abs(moment.offset), 60)
-        if moment.offset > 0 do
-          "~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w+~.2.0w:~.2.0w"
-        else
-          "~.4.0w-~.2.0w-~.2.0wT~.2.0w:~.2.0w:~.2.0w-~.2.0w:~.2.0w"
-        end
-        |> :io_lib.format([moment.year, moment.month, moment.day, moment.hour, moment.minute, moment.second,
-                           offset_h, offset_m])
-    end
-    |> :erlang.iolist_to_binary()
+    format(moment, "YYYY-MM-DDTHH:mm:ss.SSSZ")
   end
+
+  @doc """
+  Format moment according to format string.
+  """
+  def format(moment, "YYYY" <> rest), do: Integer.to_string(moment.year)          <> format(moment, rest)
+  def format(moment,   "YY" <> rest), do: format_digits(rem(moment.year, 100), 2) <> format(moment, rest)
+  def format(moment,   "MM" <> rest), do: format_digits(moment.month, 2)          <> format(moment, rest)
+  def format(moment,    "M" <> rest), do: Integer.to_string(moment.month)         <> format(moment, rest)
+  def format(moment,   "DD" <> rest), do: format_digits(moment.day, 2)            <> format(moment, rest)
+  def format(moment,    "D" <> rest), do: Integer.to_string(moment.day)           <> format(moment, rest)
+  def format(moment,   "HH" <> rest), do: format_digits(moment.hour, 2)           <> format(moment, rest)
+  def format(moment,    "H" <> rest), do: Integer.to_string(moment.hour)          <> format(moment, rest)
+  def format(moment,   "mm" <> rest), do: format_digits(moment.minute, 2)         <> format(moment, rest)
+  def format(moment,    "m" <> rest), do: Integer.to_string(moment.minute)        <> format(moment, rest)
+  def format(moment,   "ss" <> rest), do: format_digits(moment.second, 2)         <> format(moment, rest)
+  def format(moment,    "s" <> rest), do: Integer.to_string(moment.second)        <> format(moment, rest)
+
+  def format(moment, "SSSSSSSSS" <> rest) do
+    format_digits(moment.nanosecond, 9) <> format(moment, rest)
+  end
+
+  def format(moment, "SSSSSS" <> rest) do
+    format_digits(div(moment.nanosecond, 1_000), 6) <> format(moment, rest)
+  end
+
+  def format(moment, "SSS" <> rest) do
+    format_digits(div(moment.nanosecond, 1_000_000), 3) <> format(moment, rest)
+  end
+
+  def format(moment, "SS" <> rest) do
+    format_digits(div(moment.nanosecond, 10_000_000), 2) <> format(moment, rest)
+  end
+
+  def format(moment, "S" <> rest) do
+    Integer.to_string(div(moment.nanosecond, 100_000_000)) <> format(moment, rest)
+  end
+
+  def format(moment, "ZZ" <> rest) do
+    offset_h = format_digits(div(abs(moment.offset), 60), 2)
+    offset_m = format_digits(rem(abs(moment.offset), 60), 2)
+    if moment.offset >= 0 do
+      "+#{offset_h}#{offset_m}" <> format(moment, rest)
+    else
+      "-#{offset_h}#{offset_m}" <> format(moment, rest)
+    end
+  end
+
+  def format(moment, "Z" <> rest) do
+    offset_h = format_digits(div(abs(moment.offset), 60), 2)
+    offset_m = format_digits(rem(abs(moment.offset), 60), 2)
+    if moment.offset >= 0 do
+      "+#{offset_h}:#{offset_m}" <> format(moment, rest)
+    else
+      "-#{offset_h}:#{offset_m}" <> format(moment, rest)
+    end
+  end
+
+  def format(moment, << h, rest :: binary >>) do
+    << h, format(moment, rest) :: binary >>
+  end
+
+  def format(moment, "") do
+    ""
+  end
+
+  ## Private
 
   defp calc_offset(now) do
     local = now
@@ -65,5 +116,9 @@ defmodule Moment do
     |> :calendar.datetime_to_gregorian_seconds
 
     div(local , 60) - div(universal, 60)
+  end
+
+  defp format_digits(i, n \\ 2) do
+    :erlang.iolist_to_binary(:io_lib.format("~.#{n}.0w", [i]))
   end
 end
